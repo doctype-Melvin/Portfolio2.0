abbf957285d2a7d2534f92a2b37da1465eb71ed9
Aman Khemka <amankhemka.ak28@gmail.com>
2022-10-02 11:50:51 +0530

    added routing, updated image name and projects component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import Title from './Title';
 import Expertise from './Expertise';
 import Projects from './Projects';
 import Footer from './Footer';
 
+function HomePage() {
+    return (
+        <>
+            <Title />
+            <Expertise />
+            <Projects isPreview />
+            <Footer />
+        </>
+    )
+}
+
 function App() {
-    return (<div>
-        <Title />
-        <Expertise />
-        <Projects />
-        <Footer />
-    </div>);
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Outlet />}>
+                    <Route index element={<HomePage />} />
+                    <Route path='projects' element={<Projects />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,76 @@
 let projects = [
-    {
-        key: 0,
-        hrefSource: "#",
-        imageSource: "https://media.giphy.com/media/l1Oe5bzMJi5LVcbP0u/giphy.gif",
-        projectName: "Portfolio"
+  {
+    key: 0,
+    hrefSource: "#",
+    imageSource: "https://media.giphy.com/media/l1Oe5bzMJi5LVcbP0u/giphy.gif",
+    projectName: "Portfolio"
+  },
+  {
+    key: 1,
+    hrefSource: "https://veer-blogs.herokuapp.com/",
+    imageSource: "https://media.giphy.com/media/UtnxCnjWAOL1J6TNUR/giphy.gif",
+    projectName: "Veer Blogs"
+  },
+  {
+    key: 2,
+    hrefSource: "https://github.com/viralRupani/Morse-Code",
+    imageSource: "https://media.giphy.com/media/3o6ZsVLQTBtpTQ7bZC/giphy.gif",
+    projectName: "Morse Code",
+  },
+  {
+    key: 3,
+    hrefSource: "https://github.com/viralRupani/Watermarker",
+    imageSource: "images/watermarker.png",
+    projectName: "Watermarker",
+  },
+  {
+    key: 4, hrefSource: "https://github.com/viralRupani/Type-Test",
+    imageSource: "https://media.giphy.com/media/fQZX2aoRC1Tqw/giphy.gif",
+    projectName: "Type Speed Test",
+  },
+  {
+    key: 5, hrefSource: "https://cafe-wifi-restful-api-practice.herokuapp.com/",
+    imageSource: "https://media.giphy.com/media/gOQ6EgtAiwXde/giphy.gif",
+    projectName: "Cafe and Wifi(RESTful api)",
+  },
 
-    }
+  {
+    key: 6,
+    hrefSource: "https://github.com/viralRupani/Tic-Tac-Toe",
+    imageSource: "https://media.giphy.com/media/qAWWpdxRzQCFEVGbiJ/giphy.gif",
+    projectName: "Tic Tac Toe"
+  },
+  {
+    key: 7,
+    hrefSource: "https://github.com/viralRupani/Breakout-Game",
+    imageSource: "https://media.giphy.com/media/26u4iGYsaVfWljK6s/giphy.gif",
+    projectName: "Breakout Game"
+  },
+  {
+    key: 8,
+    hrefSource: "https://flask-todolists.herokuapp.com/",
+    imageSource: "https://media.giphy.com/media/B7o99rIuystY4/giphy.gif",
+    projectName: "TodoLIst"
+  },
+  {
+    key: 9,
+    hrefSource: "https://github.com/viralRupani/Disappearing-Text-Writing-App",
+    imageSource: "https://media.giphy.com/media/XGOnPgiiqcwb1JEQJN/giphy.gif",
+    projectName: "Disappearing Text Writing"
+  },
+  {
+    key: 10,
+    hrefSource: "https://flask-color-extract.herokuapp.com/",
+    imageSource: "https://media.giphy.com/media/XEgmzMLDhFQAga8umN/giphy.gif",
+    projectName: "Color Extractor",
+  },
+
+  {
+    key: 11,
+    hrefSource: "https://github.com/viralRupani/Dinosaur-automation",
+    imageSource: "https://media.giphy.com/media/iVqXV4vE4DDYk/giphy.gif",
+    projectName: "Dinosaur Game (Automation)",
+  }
 ]
 
 export default projects;
@@ -17,7 +82,7 @@ export default projects;
                <p>Veer Blogs</p>
              </a>
            </div>
-   
+
            <div className="col-12 col-md-6 col-lg-4">
              <a className="url-text" href="https://github.com/viralRupani/Morse-Code" target="_blank">
                <img className="project-img" src="https://media.giphy.com/media/3o6ZsVLQTBtpTQ7bZC/giphy.gif"
@@ -33,14 +98,14 @@ export default projects;
            </div>
            <div className="col-12 col-md-6 col-lg-4">
              <a className="url-text" href="https://github.com/viralRupani/Type-Test" target="_blank">
-               <img className="project-img" src="https://media.giphy.com/media/fQZX2aoRC1Tqw/giphy.gif" 
+               <img className="project-img" src="https://media.giphy.com/media/fQZX2aoRC1Tqw/giphy.gif"
                  alt="opps!" />
                <p>Type Speed Test</p>
              </a>
            </div>
            <div className="col-12 col-md-6 col-lg-4">
              <a className="url-text" href="https://cafe-wifi-restful-api-practice.herokuapp.com/" target="_blank">
-               <img className="project-img" src="https://media.giphy.com/media/gOQ6EgtAiwXde/giphy.gif" 
+               <img className="project-img" src="https://media.giphy.com/media/gOQ6EgtAiwXde/giphy.gif"
                  alt="opps!" />
                <p>Cafe and Wifi(RESTful api)</p>
              </a>
